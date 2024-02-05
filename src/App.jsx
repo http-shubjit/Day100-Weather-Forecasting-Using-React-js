4a95b1bdb1077ab1a565a62b51382e7f94bff45d
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-02-05 19:28:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Weatherapp from './Weatherapp/Weatherapp'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weatherapp />
+    </div>
+  )
+}
+
+export default App
